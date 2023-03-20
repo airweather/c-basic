b968c5e944d4247231741af5b8cf41cf22fa7a08
Jun <wnsghd1@naver.com>
2023-03-20 22:00:00 +0000

    주석 일부 추가

--- data-type-print.c
+++ data-type-print.c
@@ -6,7 +6,7 @@ void main()
     float f = 123.4567;
     char s[] ="Happy birthday";
     printf("정수형 자료의 출력 \n");
-    printf("\n %04d \n \t %09d \n\n", i, i);
+    printf("\t %04d \n \t %09d \n\n", i, i);
     printf("실수형 자료의 출력 \n");
     printf("\t %.3f \n \t %.3e \n\n", f, f);
     printf("문자열 자료의 출력 \n");
